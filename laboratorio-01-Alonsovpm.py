3e6325efb3af445dde758a10f62adde51cc7ca05
Alonsovpm <avpizarro@miuandes.cl>
2021-03-22 19:18:27 -0300

    lista tablero 2

--- laboratorio-01-Alonsovpm.py
+++ laboratorio-01-Alonsovpm.py
@@ -11,34 +11,36 @@ def generador_cartas(numero_cartas):
     return lista_cartas
 
 numero_cartas = int(input("Con cuantas cartas desea jugar: "))
-print((generador_cartas(numero_cartas)))
-print(len((generador_cartas(numero_cartas))))
+
+lista_cartas = generador_cartas(numero_cartas)
+
+print(lista_cartas)
+print(len((lista_cartas)))
 n=0
 m=0
 
-if len(generador_cartas(numero_cartas))%2==0:
-    n=int(len(generador_cartas(numero_cartas))/2)
+if len(lista_cartas)%2==0:
+    n=int(len(lista_cartas)/2)
     m=2
-if len(generador_cartas(numero_cartas))%3==0:
-    n=int(len(generador_cartas(numero_cartas))/3)
+if len(lista_cartas)%3==0:
+    n=int(len(lista_cartas)/3)
     m=3
-if len(generador_cartas(numero_cartas))%4==0:
-    n=int(len(generador_cartas(numero_cartas))/4)
+if len(lista_cartas)%4==0:
+    n=int(len(lista_cartas)/4)
     m=4
-if len(generador_cartas(numero_cartas))%5==0:
-    n=int(len(generador_cartas(numero_cartas))/5)
+if len(lista_cartas)%5==0:
+    n=int(len(lista_cartas)/5)
     m=5
-    
-print(n) 
 
+print(n) 
 lista_tablero=[]
-for i in range(m):
+for i in range(n):
     lista_tablero.append([])
 print(lista_tablero)
 
-for i in range(m):
-    for j in range(n):
-        lista_tablero[i].append(generador_cartas(numero_cartas)[j])
+for i in range(n):
+    for j in range(m):
+        lista_tablero[i].append(lista_cartas[j])
 print(lista_tablero)
 
 
