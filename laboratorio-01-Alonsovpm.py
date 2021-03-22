61befb566079b37721e675fc491b19f60b5c5aa5
Alonsovpm <avpizarro@miuandes.cl>
2021-03-22 16:06:17 -0300

    generador cartas 1

--- laboratorio-01-Alonsovpm.py
+++ laboratorio-01-Alonsovpm.py
@@ -1,1 +1,9 @@
-numero_de_cartas: input("Con cuantas cartas desea jugar: ")
+import random
+
+def generador_cartas(numero_cartas):
+    lista_cartas: []
+    for i in range(numero_cartas):
+        n = random.randint(1,numero_cartas)
+        lista_cartas.append(n)
+
+numero_cartas: input("Con cuantas cartas desea jugar: ")
