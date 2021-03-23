4d2c153fdf6448f94b51de464a0266a5ddad5370
Alonsovpm <avpizarro@miuandes.cl>
2021-03-23 11:46:30 -0300

    arreglo lista cartas

--- laboratorio-01-Alonsovpm.py
+++ laboratorio-01-Alonsovpm.py
@@ -3,8 +3,7 @@ import random
 def generador_cartas(numero_cartas):
     lista_cartas = []
     for i in range(numero_cartas):
-        n = random.randint(1,numero_cartas)
-        lista_cartas.append(n)
+        lista_cartas.append(i+1)
     for i in range(len(lista_cartas)):
         lista_cartas.append(lista_cartas[i])
     random.shuffle(lista_cartas)
@@ -14,8 +13,6 @@ numero_cartas = int(input("Con cuantas cartas desea jugar: "))
 
 lista_cartas = generador_cartas(numero_cartas)
 
-print(lista_cartas)
-print(len((lista_cartas)))
 n=0
 m=0
 
@@ -32,21 +29,22 @@ if len(lista_cartas)%5==0:
     n=int(len(lista_cartas)/5)
     m=5
 
-print(n) 
+
 lista_tablero=[]
 for i in range(n):
     lista_tablero.append([])
-print(lista_tablero)
+
 k=0
 for i in range(n):
     for j in range(m):
         lista_tablero[i].append(lista_cartas[j+k])
     k+=m
-print(lista_tablero)
 
+
+#imprimir tablero
 for i in range(len(lista_tablero)):
     for j in lista_tablero[i]:
-        print(j, end="")
+        print(j, end=" ")
     print("")
 
 
